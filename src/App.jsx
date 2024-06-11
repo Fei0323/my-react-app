24b49583f2c94065ad41f4e22bea2a91f6a23391
Fei0323 <fei.bao.guo@gmail.com>
2024-06-11 10:35:46 +0200

    challenge02

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/*
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -33,3 +34,17 @@ function App() {
 }
 
 export default App
+*/
+
+import React from 'react';
+import PokemonCard from './components/PokemonCard';
+
+const App = () => {
+  return (
+    <div>
+      <PokemonCard />
+    </div>
+  );
+};
+
+export default App;
