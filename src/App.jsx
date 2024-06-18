51e5f11c2b12ba3dc7f2af2e8b7158ced822be6a
Fei0323 <fei.bao.guo@gmail.com>
2024-06-18 11:09:46 +0200

    challenge06usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PokemonCard from './components/PokemonCard';
 
 const pokemonList = [
   {
     name: "bulbasaur",
-    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
   },
   {
     name: "mew",
   },
 ];
 
-const App = () => {
+function App() {
+  const [pokemonIndex, setPokemonIndex] = useState(0);
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[0]} />
+      {pokemonIndex > 0 && (
+        <button onClick={() => setPokemonIndex(pokemonIndex - 1)}>before</button>
+      )}
+      {pokemonIndex < pokemonList.length - 1 && (
+        <button onClick={() => setPokemonIndex(pokemonIndex + 1)}>after</button>
+      )}
+      <div>
+        <h1>{pokemonList[pokemonIndex].name}</h1>
+        <img src={pokemonList[pokemonIndex].imgSrc} alt={pokemonList[pokemonIndex].name} />
+      </div>
     </div>
   );
 };
